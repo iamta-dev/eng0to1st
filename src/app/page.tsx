825452abebfb12ae4bdab0ceaad30760e767d5d6
Natthawat Narin <natthawat.narin@gmail.com>
2024-06-09 22:23:50 +0700

    init

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,3 @@
-import { DarkThemeToggle, Tooltip } from "flowbite-react";
 import { CreatePost } from "~/app/_components/game";
 import { LogoBrand } from "~/app/_components/logo-brand";
 
@@ -11,17 +10,6 @@ export default async function Home() {
         </div>
 
         <CreatePost />
-
-        <div className="hidden dark:block">
-          <Tooltip content="Toggle light mode">
-            <DarkThemeToggle />
-          </Tooltip>
-        </div>
-        <div className="dark:hidden">
-          <Tooltip content="Toggle dark mode">
-            <DarkThemeToggle />
-          </Tooltip>
-        </div>
       </div>
     </main>
   );
